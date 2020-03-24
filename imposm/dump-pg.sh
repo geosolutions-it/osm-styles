64e5f6d340ee502ed195c0d26f0c24899a6a91f5
Simon <simdevmon@gmail.com>
2020-03-24 10:46:52 +0100

    Set imposm version in variable

--- imposm/dump-pg.sh
+++ imposm/dump-pg.sh
@@ -10,6 +10,8 @@ PG_IMAGE_VERSION=$PG_IMAGE_VERSION_DEFAULT
 PG_CONTAINER_REMOVE_DEFAULT=true
 PG_CONTAINER_REMOVE=$PG_CONTAINER_REMOVE_DEFAULT
 
+IMPOSM_VERSION=0.10.0
+
 # Parse input parameters
 set -e
 while getopts p::c::v::r::i: option
@@ -46,8 +48,8 @@ mkdir -p work
 docker run --name "$PG_CONTAINER" -p $PG_PORT:5432 -d  -v `pwd`/work:/tmp/work -t kartoza/postgis:$PG_IMAGE_VERSION
 
 echo -e "\n----------- Downloading and unpacking Imposm 3"
-wget -q --show-progress -c https://github.com/omniscale/imposm3/releases/download/v0.10.0/imposm-0.10.0-linux-x86-64.tar.gz -P work
-tar -xzf work/imposm-0.10.0-linux-x86-64.tar.gz -C work
+wget -q --show-progress -c https://github.com/omniscale/imposm3/releases/download/v${IMPOSM_VERSION}/imposm-${IMPOSM_VERSION}-linux-x86-64.tar.gz -P work
+tar -xzf work/imposm-${IMPOSM_VERSION}-linux-x86-64.tar.gz -C work
 echo "Done"
 
 echo -e "\n----------- Waiting for PostgreSQL to be up and running"
@@ -61,13 +63,13 @@ done
 echo -e "\n----------- Running imposm, read from pbf"
 mkdir -p work/tmp
 rm -rf work/tmp/*
-work/imposm-0.10.0-linux-x86-64/imposm import -mapping mapping.yml -read $PBF_LOCATION -cachedir work/tmp 
+work/imposm-${IMPOSM_VERSION}-linux-x86-64/imposm import -mapping mapping.yml -read $PBF_LOCATION -cachedir work/tmp 
 echo -e "\n----------- Running imposm, write to database"
-work/imposm-0.10.0-linux-x86-64/imposm import -mapping mapping.yml -cachedir work/tmp -write -connection "postgis://docker:docker@localhost:$PG_PORT/gis"
+work/imposm-${IMPOSM_VERSION}-linux-x86-64/imposm import -mapping mapping.yml -cachedir work/tmp -write -connection "postgis://docker:docker@localhost:$PG_PORT/gis"
 echo -e "\n----------- Deploy imported tables to production"
-work/imposm-0.10.0-linux-x86-64/imposm import -mapping mapping.yml -connection "postgis://docker:docker@localhost:$PG_PORT/gis" -deployproduction
+work/imposm-${IMPOSM_VERSION}-linux-x86-64/imposm import -mapping mapping.yml -connection "postgis://docker:docker@localhost:$PG_PORT/gis" -deployproduction
 echo -e "\n----------- Remove backup scheme"
-work/imposm-0.10.0-linux-x86-64/imposm import -mapping mapping.yml -connection "postgis://docker:docker@localhost:$PG_PORT/gis" -removebackup
+work/imposm-${IMPOSM_VERSION}-linux-x86-64/imposm import -mapping mapping.yml -connection "postgis://docker:docker@localhost:$PG_PORT/gis" -removebackup
 
 echo -e "\n----------- Dumping the backup"
 if [ $UID ]; then
